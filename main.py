6cff70169cca512c4d5145ffe228b93e053f5ca7
Dominik <152535653+Domiko7@users.noreply.github.com>
2025-01-26 21:40:24 +0100

    made the code more professional but not tested yet so i recommend to go to the old version

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ import os
 import ctypes
 from playsound import playsound
 from plyer import notification
+NoW = False
 
-def EEWsound(laungage):
-    if laungage == 'English':
+def EEWsound(language):
+    if language == 'English':
         playsound('sounds/Shaking(EN).mp3')
-    elif laungage == 'Japanese':
+    elif language == 'Japanese':
         playsound('sounds/Shaking(JP).mp3')
-    elif laungage == 'Chinese':
+    elif language == 'Chinese':
         playsound('sounds/Shaking(CH).mp3')
-    elif laungage == 'Spanish':
+    elif language == 'Spanish':
         playsound('sounds/Shaking(ES).mp3')
-    elif laungage == 'Korean':
+    elif language == 'Korean':
         playsound('sounds/Shaking(SK).mp3')
-    elif laungage == 'Polish':
+    elif language == 'Polish':
         playsound('sounds/Shaking(PL).mp3')
     playsound('sounds/Emergency_Alert02-1.mp3')
     #playsound('sounds/Emergency_Alert01-1.mp3')
@@ -28,77 +29,98 @@ def EEWsound(laungage):
         timeout=5  # Duration in seconds
     )
 
-NoW=False
-def on_message(ws, message):
-    global toaster
-    global NoW
-    laungage = 'Chinese'
-    data = json.loads(message)
-    if data.get("type") == "jma_eew":
-        if data.get("isFinal"):
+class jmaEEW:
+    global EEWsound
+    def __init__(self, data, language):
+        self.data = data
+        self.language = language
+    def displayAlert(self):
+        if self.data.get("isFinal"):
             print("THIS IS THE FINAL REPORT")
         print("Earthquake Alert! (JAPAN EEW)")
-        print(f"Title: {data.get('Title')}")
-        print(f"Location: {data.get('Hypocenter')}")
-        print(f"Magnitude: {data.get('Magunitude')}")
-        print(f"Max Intensity: {data.get('MaxIntensity')}")
-        print(f"Maximum earthquake intensity: {data.get('WarnArea').get('Shindo1')}")
-        print(f"Orgin time: {data.get('OriginTime')}")
-        print(f"Announced time: {data.get('AnnouncedTime')}")
-        print(f"Depth: {data.get('Depth')}")
-        print(f"Warning area arrival: {data.get('WarnArea').get('Arrive')}")
-        print(f"Warning Areas: {data.get('WarnArea').get('Chiiki')}")
-        print(f"Method: {data.get('isAssumption')}")
-        EEWsound(laungage)
-        NoW = False
-        if data.get("isCancel"):
+        print(f"Title: {self.data.get('Title')}")
+        print(f"Location: {self.data.get('Hypocenter')}")
+        print(f"Magnitude: {self.data.get('Magunitude')}")
+        print(f"Max Intensity: {self.data.get('MaxIntensity')}")
+        print(f"Maximum earthquake intensity: {self.data.get('WarnArea').get('Shindo1')}")
+        print(f"Origin time: {self.data.get('OriginTime')}")
+        print(f"Announced time: {self.data.get('AnnouncedTime')}")
+        print(f"Depth: {self.data.get('Depth')}")
+        print(f"Warning area arrival: {self.data.get('WarnArea').get('Arrive')}")
+        print(f"Warning Areas: {self.data.get('WarnArea').get('Chiiki')}")
+        print(f"Method: {self.data.get('isAssumption')}")
+        if self.data.get("isCancel"):
             for i in range(5):
                 print('FALSE EEW ALARM')
-    elif data.get("type") == "cwa_eew":
+        EEWsound(self.language)
+class cwaEEW:
+    global EEWsound
+    def __init__(self, data, language):
+        self.data = data
+        self.language = language
+    def displayAlert(self):
         print("Earthquake Alert! (CWA EEW)")
-        print(f"Magnitude: {data.get('Magunitude')}")
-        print(f"ID: {data.get('ID')}")
-        print(f"Depth: {data.get('Depth')}")
-        print(f"Time: {data.get('OrginTime')}")
-        print(f"Location: {data.get('HypoCenter')}")
-        print(f"Maximum intensity: {data.get('MaxIntensity')}")
-        EEWsound(laungage)
-        NoW = False
-        print(f"ReportTIme: {data.get('ReportTime')}")
-    elif data.get("type") == "sc_eew":
+        print(f"Magnitude: {self.data.get('Magunitude')}")
+        print(f"ID: {self.data.get('ID')}")
+        print(f"Depth: {self.data.get('Depth')}")
+        print(f"Time: {self.data.get('OrginTime')}")
+        print(f"Location: {self.data.get('HypoCenter')}")
+        print(f"Maximum intensity: {self.data.get('MaxIntensity')}")
+        EEWsound(self.language)
+
+class scEEW:
+    global EEWsound
+    def __init__(self, data, language):
+        self.data = data
+        self.language = language
+    def displayAlert(self):
         print("Earthquake Alert! (SC EEW)")
-        print(f"Magnitude: {data.get('Magunitude')}")
-        print(f"ID: {data.get('ID')}")
-        print(f"Depth: {data.get('Depth')}")
-        print(f"Time: {data.get('OrginTime')}")
-        print(f"Location: {data.get('hypoCenter')}")
-        print(f"Maximum intensity: {data.get('MaxIntensity')}")
-        EEWsound(laungage)
-        NoW = False
-    elif data.get("type") == "fj_eew":
+        print(f"Magnitude: {self.data.get('Magunitude')}")
+        print(f"ID: {self.data.get('ID')}")
+        print(f"Depth: {self.data.get('Depth')}")
+        print(f"Time: {self.data.get('OrginTime')}")
+        print(f"Location: {self.data.get('hypoCenter')}")
+        print(f"Maximum intensity: {self.data.get('MaxIntensity')}")
+        EEWsound(self.language)
+
+class fjEEW:
+    global EEWsound
+    def __init__(self, data, language):
+        self.data = data
+        self.language = language
+    def displayAlert(self):
         print("Earthquake Alert! (FJ EEW)")
-        print(f"Magnitude: {data.get('Magunitude')}")
-        print(f"ID: {data.get('ID')}")
-        print(f"Depth: {data.get('Depth')}")
-        print(f"Time: {data.get('OrginTime')}")
-        print(f"Location: {data.get('HypoCenter')}")
-        print(f"Maximum intensity: {data.get('maxIntensity')}")
-        print(f"ReportTIme: {data.get('ReportTime')}")
-        print(f"Final: {data.get('isFinal')}")
-        EEWsound(laungage)
-        NoW = False
-    elif data.get("type") == "cenc_eqlist":
+        print(f"Magnitude: {self.data.get('Magunitude')}")
+        print(f"ID: {self.data.get('ID')}")
+        print(f"Depth: {self.data.get('Depth')}")
+        print(f"Time: {self.data.get('OrginTime')}")
+        print(f"Location: {self.data.get('HypoCenter')}")
+        print(f"Maximum intensity: {self.data.get('maxIntensity')}")
+        print(f"ReportTIme: {self.data.get('ReportTime')}")
+        print(f"Final: {self.data.get('isFinal')}")
+        EEWsound(self.language)
+
+class cencEqList:
+    global EEWsound
+    def __init__(self, data, language):
+        self.data = data
+        self.language = language
+    def displayAlert(self):
         print("Earthquake Alert! (CENC EQLIST)")
-        for key, report in data.items():
+        for key, report in self.ata.items():
             if key.startswith('No'):  
                 print(f"Earthquake Report {key}:")
-                print(f"Magnitude: {report.get('magnitude')}")
-                print(f"Depth: {report.get('depth')}")
-                print(f"Time: {report.get('time')}")
-                print(f"Location: {report.get('location')}")
-                EEWsound(laungage)
+                print(f"Magnitude: {self.report.get('magnitude')}")
+                print(f"Depth: {self.report.get('depth')}")
+                print(f"Time: {self.report.get('time')}")
+                print(f"Location: {self.report.get('location')}")
+                EEWsound(self.language)
                 NoW = False
-    elif NoW == False:
+class noEEW:
+    global EEWsound
+    def __init__(self):
+        pass
+    def noAlert(self):
         print("No EEW issued")
         notification.notify(
         title="NO EEW ISSUED",
@@ -106,6 +128,35 @@ def on_message(ws, message):
         app_icon='images/Sesnaquake3.ico',  # Path to a custom icon file (.ico). Set to None for no icon.
         timeout=2  # Duration in seconds
         )
+        
+
+def on_message(ws, message):
+    global NoW
+    language = 'Chinese'
+    data = json.loads(message)
+    if data.get("type") == "jma_eew":
+        alert = jmaEEW(data, language)
+        alert.displayAlert()
+        NoW = False
+    elif data.get("type") == "cwa_eew":
+        alert = cwaEEW(data, language)
+        alert.displayAlert()
+        NoW = False
+    elif data.get("type") == "sc_eew":
+        alert = scEEW(data, language)
+        alert.displayAlert()
+        NoW = False
+    elif data.get("type") == "fj_eew":
+        alert = fjEEW(data, language)
+        alert.displayAlert()
+        NoW = False
+    elif data.get("type") == "cenc_eqlist":
+        alert = cencEqList(data, language)
+        alert.displayAlert()
+        NoW = False
+    elif NoW == False:
+        alert = noEEW()
+        alert.noAlert()
         NoW = True
 
 # Function for handling errors
